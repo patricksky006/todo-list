d35ea6f30041bb27d322ac0bab10d32d0c3170cd
patricksky006 <patrick.sky@hotmail.com>
2023-08-28 20:40:31 +0800

    add error handling

--- app.js
+++ app.js
@@ -27,73 +27,150 @@ app.get('/', (req,res) => {
 })
 
 app.get('/todos', (req, res) => {
-  const message = req.flash('success')
-  const deleteSuccess = req.flash('deleteSuccess')
-  return Todo.findAll({
-    attributes: ['id', 'name', 'isComplete'],
-    raw: true
-  })
-      .then((todos) => res.render('todos', { todos, message, deleteSuccess })) //顯示todo清單的頁面
-      .catch((err) => res.status(422).json(err))
+  try {
+    const message = req.flash('success')
+    const deleteSuccess = req.flash('deleteSuccess')
+    const deletedError = req.flash('deletedError')
+
+     return Todo.findAll({
+        attributes: ['id', 'name', 'isComplete'],
+        raw: true
+     })
+        .then((todos) => res.render('todos', { todos, message, deleteSuccess, deletedError })) //顯示todo清單的頁面
+        .catch((error) => {
+          console.error(error)
+          req.flash('error','資料取得失敗')
+          return res.redirect('back')
+        })
+  } catch (error) {
+        console.error(error)
+	    	req.flash('error', '伺服器錯誤')
+		    return res.redirect('back')
+  }
+  
 })
 
 app.get('/todos/new', (req, res) => {
-  return res.render('new')
+  try {
+    return res.render('new', { error: req.flash('error')})
+  } catch (error) {
+        console.error(error)
+	    	req.flash('error', '伺服器錯誤')
+		    return res.redirect('back')
+  }
+  
 })
 
 app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const updatedSuccess = req.flash('updatedSuccess')
+  try {
+    const id = req.params.id
+    const updatedSuccess = req.flash('updatedSuccess')
   
-  return Todo.findByPk(id, {
-    attributes: ['id', 'name', 'isComplete'],
-    raw: true
+    return Todo.findByPk(id, {
+        attributes: ['id', 'name', 'isComplete'],
+        raw: true
   })
     .then((todo) => res.render('todo', { todo, updatedSuccess }))
-    .catch((err) => res.status(422).json(err))
+    .catch((error) => {
+        console.error(error)
+				req.flash('error', '資料取得失敗:(')
+				return res.redirect('back')
+    })
+  } catch (error) {
+        console.error(error)
+		    req.flash('error', '伺服器錯誤')
+	    	return res.redirect('back')
+  }
+  
 })
 
 app.post('/todos', (req, res) => {
-  const name = req.body.name //接住從new.hbs中的提交的資料
-  return Todo.create( { name } ) // 將資料放入在Todo資料庫中
+  try {
+    const name = req.body.name //接住從new.hbs中的提交的資料
+    return Todo.create( { name } ) // 將資料放入在Todo資料庫中
       .then(() => {
         req.flash('success', `新增資料名稱 ${ name } 成功`) //前者是Key，後者是值
         return res.redirect('/todos')
       }) 
-      .catch((err) => console.log(err))
+      .catch((error) => {
+        console.log(error)
+        req.flash('error', '新增失敗!')
+        return res.redirect('back')
+      })
+  } catch (error) {
+    console.log(error)
+    req.flash('error', '新增失敗:(')
+    return res.redirect('back')
+  }
+  
 })
 
 app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
+  try {
+    const id = req.params.id
+    const updatedError = req.flash('updatedError')
 
   return Todo.findByPk(id, {
     attributes: ['id', 'name', 'isComplete'],
     raw: true
   })
-      .then((todo) => res.render('edit', { todo }))
+      .then((todo) => res.render('edit', { todo, updatedError }))
+      .catch((error) => {
+        console.error(error)
+				req.flash('error', '資料取得失敗:(')
+				return res.redirect('back')
+      })
+  } catch (error) {
+    console.error(error)
+		req.flash('error', '伺服器錯誤')
+		return res.redirect('back')
+  }
+  
   
 })
 
 app.put('/todos/:id', (req, res) =>{
-  const { name, isComplete } = req.body
-  const id = req.params.id // 接住網頁提交更改資料的ID
+  try {
+    const { name, isComplete } = req.body
+    const id = req.params.id // 接住網頁提交更改資料的ID
 
-  return Todo.update({ name, isComplete: isComplete === 'completed' }, {where: { id }})
-    .then(() => {
-      req.flash('updatedSuccess',`更新資料名稱成功`)
-      return res.redirect(`/todos/${id}`)
+     return Todo.update({ name, isComplete: isComplete === 'completed' }, {where: { id }})
+       .then(() => {
+          req.flash('updatedSuccess',`更新資料名稱成功`)
+          return res.redirect(`/todos/${id}`)
   })
+       .catch((error) => {
+          console.log(error)
+          req.flash('updatedError', '更新失敗!')
+          return res.redirect('back')
+      })
+  } catch (error) {
+    console.log(error)
+    req.flash('error', '伺服器錯誤')
+    return res.redirect('back')
+  }
+  
 })
 
 app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-
-  return Todo.destroy({ where: { id }})
-    .then(() => {
-      req.flash('deleteSuccess',`刪除資料成功`)
-      return res.redirect('/todos')
+  try {
+    const id = req.params.id
+    return Todo.destroy({ where: { id }})
+      .then(() => {
+       req.flash('deleteSuccess',`刪除資料成功`)
+        return res.redirect('/todos')
+       })
+      .catch((error) => {
+          console.log(error)
+          req.flash('deletedError', '刪除失敗!')
+          return res.redirect('back')
   })
-})
+  } catch (error) {
+    console.log(error)
+    req.flash('error', '刪除失敗:(')
+    return res.redirect('back')
+  }
+  })
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`) //監聽伺服器
